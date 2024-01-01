2a318c92a6c28dbe762207545f83e2b432ee8238
20pa5a1210 <20pa5a1210@vishnu.edu.in>
2024-01-01 20:32:37 +0530

    feat: Landing Page

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -1,43 +1,21 @@
 package server
 
 import (
-	"encoding/json"
-	"log"
+	"hotel-checkin/cmd/web"
 	"net/http"
 
 	"github.com/a-h/templ"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
-	"hotel-checkin/cmd/web"
 )
 
 func (s *Server) RegisterRoutes() http.Handler {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 
-	r.Get("/", s.HelloWorldHandler)
-	r.Get("/health", s.healthHandler)
 	fileServer := http.FileServer(http.FS(web.Files))
 	r.Handle("/js/*", fileServer)
-	r.Get("/web", templ.Handler(web.HelloForm()).ServeHTTP)
-	r.Post("/hello", web.HelloWebHandler)
-
+	r.Get("/", templ.Handler(web.Homepage()).ServeHTTP)
+    r.Get("/login", templ.Handler(web.LoginForm()).ServeHTTP)
 	return r
-}
-
-func (s *Server) HelloWorldHandler(w http.ResponseWriter, r *http.Request) {
-	resp := make(map[string]string)
-	resp["message"] = "Hello World"
-
-	jsonResp, err := json.Marshal(resp)
-	if err != nil {
-		log.Fatalf("error handling JSON marshal. Err: %v", err)
-	}
-
-	_, _ = w.Write(jsonResp)
-}
-
-func (s *Server) healthHandler(w http.ResponseWriter, r *http.Request) {
-	jsonResp, _ := json.Marshal(s.db.Health())
-	_, _ = w.Write(jsonResp)
 }
