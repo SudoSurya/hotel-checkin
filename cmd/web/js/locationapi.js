f59535e064c8d876ecab2700cfda3872e9c7a690
20pa5a1210 <20pa5a1210@vishnu.edu.in>
2024-01-03 20:17:38 +0530

    fix: location api limited country to IN

--- cmd/web/js/locationapi.js
+++ cmd/web/js/locationapi.js
@@ -1,43 +1,3 @@
-async function getCountries() {
-    try {
-        const response = await fetch("https://api.countrystatecity.in/v1/countries", {
-            method: 'GET',
-            headers: {
-                'X-CSCAPI-KEY': 'd3ZwTnBXVERmSXlMNzd3dlc5SXBRS29uR2plcEVVeGxwZ3lWQzhvQg==', // Replace with your API key
-            },
-        });
-
-        if (!response.ok) {
-            throw new Error('Failed to fetch countries');
-        }
-
-        const data = await response.json();
-        const countriesDropdown = document.getElementById("country");
-
-        // Clear existing options
-        countriesDropdown.innerHTML = "";
-
-        // Add a default option
-        const defaultOption = document.createElement("option");
-        defaultOption.value = ""; // Set an appropriate default value
-        defaultOption.text = "Select Country"; // Set an appropriate default text
-        countriesDropdown.appendChild(defaultOption);
-
-        // Add options for each country
-        data.forEach(country => {
-            const option = document.createElement("option");
-            option.value = country.iso2; // Set the value to the country ID or relevant identifier
-            option.text = country.name; // Set the text to the country name
-            countriesDropdown.appendChild(option);
-        });
-
-        // Trigger the updateStates function to populate the states dropdown based on the selected country
-        // updateStates();
-    } catch (error) {
-        console.error('Error fetching countries:', error);
-    }
-}
-
 async function getStates(country) {
     try {
         const response = await fetch(`https://api.countrystatecity.in/v1/countries/${country}/states`, {
@@ -81,14 +41,11 @@ async function getCities(state, country) {
             },
         });
 
-        console.log("in get cities");
-
         if (!response.ok) {
             throw new Error(`Failed to fetch cities. Status: ${response.status} ${response.statusText}`);
         }
 
         const data = await response.json();
-        console.log("Cities data:", data);
 
         const citiesDropdown = document.getElementById("city");
 
@@ -108,18 +65,14 @@ async function getCities(state, country) {
 }
 function updateStates() {
     var selectedCountry = document.getElementById("country").value;
-    console.log(selectedCountry);
     getStates(selectedCountry);
 }
 
 function updateCities() {
     var selectedState = document.getElementById("state").value;
     var selectedCountry = document.getElementById("country").value;
-    console.log(selectedState);
-    console.log(selectedCountry);
     getCities(selectedState, selectedCountry);
 }
-getCountries();
 
 document.addEventListener("DOMContentLoaded", function() {
     var form = document.getElementById("hotelrefform");
