eabf5b20f5cd0c7181062ac3fcda8c1871bcbf48
20pa5a1210 <20pa5a1210@vishnu.edu.in>
2024-01-05 23:16:43 +0530

    feat: new migrations

---
+++ migrations/20240103165638_insertadmin.sql
@@ -0,0 +1,10 @@
+-- +goose Up
+-- +goose StatementBegin
+INSERT INTO admin (name, email, password, department, isAuthorized, created_at, updated_at) VALUES
+                  ('admin', 'admin@example.com', 'admin', 'FBI', true, '2020-01-03 16:56:38', '2020-01-03 16:56:38');
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DELETE FROM admin WHERE email = 'admin@example.com';
+-- +goose StatementEnd
